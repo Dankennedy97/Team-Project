188ae76ee1e48a3ca81cc7dcc436c897a74ade70
Dankennedy97 <danielkennedy496@yahoo.ie>
2018-11-21 11:32:06 +0000

    Initial commit

---
+++ Home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="shortcut icon" href="images/tplogo.PNG">
+		<title>
+		Schedule Solutions
+		</title>
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/tp.css" />
+		<link rel="stylesheet" href="tp.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="sss/sss.min.js"></script>
+		<link rel="stylesheet" href="sss/sss.css" type="text/css" media="all">
+		<script>jQuery(function($) {$('.slider').sss();});</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
+		<ul id="list1" class="nav navbar-nav navbar-right">
+					<li><a href="Home.php">Home</a></li>
+					<li><a href="details.php">Details</a></li>
+					<li><a href="class.php">Class + CA Input</a></li>
+					<li><a href="timetable.php">Timetable</a></li>
+		</ul>
+		<ul id="list2">
+			<li><a href="index.php">Login</a></li>
+			<li><a href="register.php">Register</a></li>
+		</ul>
+		
+	</head>
+	<body>
+		<div id="logo">
+			<img src="images/tplogo"/>
+		</div>
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="shortcut icon" href="images/tplogo.PNG">
+		<title>
+		Schedule Solutions
+		</title>
+		<meta name="viewport" content="width=device-width, initial scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/tp.css" />
+		<link rel="stylesheet" href="tp.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="sss/sss.min.js"></script>
+		<link rel="stylesheet" href="sss/sss.css" type="text/css" media="all">
+		<script>jQuery(function($) {$('.slider').sss();});</script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
+		<ul class="nav navbar-nav navbar-right">
+					<li><a href="Home.php">Home</a></li>
+					<li><a href="class.php">Class + CA Input</a></li>
+					<li><a href="timetable.php">Timetable</a></li>
+		</ul>
+	
+	</head>
+	<body>
+		<div id="box3" class ="style1">
+		  <form action="/action_page.php">
+			  Username: <input type="text" name="Username" value=""><br>
+			  Password: <input type="text" name="Password" value=""><br>
+			  Name: <input type="text" name="Name" value=""><br>
+			  Email: <input type="text" name="Email" value=""><br>
+			  Address: <input type="text" name="Address" value=""><br>
+			  Course: <input type="text" name="Course" value=""><br>
+			  <input type="submit" value="Submit">
+		  </form>
+		</div>
+	</body>
+	<?php
+	$user = 'root';
+	$pass = '';
+	$rdb = 'student';
+  
+	$connect = new mysqli('localhost', $user, $pass, $rdb) or die("Unable to connect.");
+	?>
+</html>
